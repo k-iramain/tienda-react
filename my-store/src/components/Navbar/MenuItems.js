6be920e4700f56ca77eadd1638f561185b9ee52c
karina iramain <karina.iramain90@gmail.com>
2022-01-10 17:31:58 -0300

    desafio navbar

---
+++ my-store/src/components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title :"Home"  ,
+        URL : "#",
+        cName : "nav-links"
+    },
+    {
+        title : "Services",
+        URL : "#",
+        cName : "nav-links"
+    },
+    {
+        title : "Products" ,
+        URL : "#",
+        cName : "nav-links"
+    },
+    {
+        title : "Contact us",
+        URL : "#",
+        cName : "nav-links"
+    },
+    
+]
